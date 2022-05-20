3f6d9ba72c704f07d872467a22ca8e7016a0a6d5
Tryzack <44532224+Tryzack@users.noreply.github.com>
2022-05-20 12:06:24 -0400

    Create Operaciones.java

---
+++ PruebasPersonales/Calculadora/Operaciones.java
@@ -0,0 +1,26 @@
+package Calculadora;
+
+public class Operaciones {
+
+	    double answer;
+
+	    public void suma(double a, double b){
+	        answer = a + b;
+	        System.out.println(answer);
+	    }
+
+	    public void resta(double a, double b){
+	        answer = a - b;
+	        System.out.println(answer);
+	    }
+
+	    public void division(double a, double b){
+	        answer = a / b;
+	        System.out.println(answer);
+	    }
+
+	    public void multiplicacion(double a, double b){
+	        answer = a * b;
+	        System.out.println(answer);
+	    }
+}
