4ea253ea47f2ab4781dd9db633bbc828e237ba13
Tryzack <44532224+Tryzack@users.noreply.github.com>
2022-05-20 12:12:47 -0400

    Evitar divisiones entre 0

--- PruebasPersonales/Calculadora/Operaciones.java
+++ PruebasPersonales/Calculadora/Operaciones.java
@@ -6,21 +6,24 @@ public class Operaciones {
 
 	    public void suma(double a, double b){
 	        answer = a + b;
-	        System.out.println(answer);
+	        System.out.println("La respuesta es"+answer);
 	    }
 
 	    public void resta(double a, double b){
 	        answer = a - b;
-	        System.out.println(answer);
+	        System.out.println("La respuesta es"+answer);
 	    }
 
 	    public void division(double a, double b){
-	        answer = a / b;
-	        System.out.println(answer);
+	    	if(b!=0) {
+	    		answer = a / b;
+	    		System.out.println("La respuesta es"+answer);
+	    	}else{
+	    		System.out.println("No se puede dividir entre 0");
+	    	}
 	    }
-
 	    public void multiplicacion(double a, double b){
 	        answer = a * b;
-	        System.out.println(answer);
+	        System.out.println("La respuesta es"+answer);
 	    }
 }
