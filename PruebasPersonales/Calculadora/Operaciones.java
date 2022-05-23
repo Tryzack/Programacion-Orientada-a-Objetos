6417ac542c0b8edadb7709dd5797a760c52baac2
Tryzack <44532224+Tryzack@users.noreply.github.com>
2022-05-22 21:56:23 -0400

    Update Operaciones.java

--- PruebasPersonales/Calculadora/Operaciones.java
+++ PruebasPersonales/Calculadora/Operaciones.java
@@ -6,24 +6,25 @@ public class Operaciones {
 
 	    public void suma(double a, double b){
 	        answer = a + b;
-	        System.out.println("La respuesta es"+answer);
+	        System.out.println("La respuesta es: "+answer);
 	    }
 
 	    public void resta(double a, double b){
 	        answer = a - b;
-	        System.out.println("La respuesta es"+answer);
+	        System.out.println("La respuesta es: "+answer);
 	    }
 
 	    public void division(double a, double b){
 	    	if(b!=0) {
 	    		answer = a / b;
-	    		System.out.println("La respuesta es"+answer);
+	    		System.out.println("La respuesta es: "+answer);
 	    	}else{
 	    		System.out.println("No se puede dividir entre 0");
 	    	}
 	    }
+
 	    public void multiplicacion(double a, double b){
 	        answer = a * b;
-	        System.out.println("La respuesta es"+answer);
+	        System.out.println("La respuesta es: "+answer);
 	    }
 }
